f8568183e75ec076b97df560ef1963e8ee7e90f3
Pushkin Sergey <pushkinsergey.voronezh@gmail.com>
2023-09-22 17:59:46 +0300

    feat: Exclude data source auto configuration for test context

--- src/test/java/com/iexapis/cloud/integration/concurrencystock/ConcurrencyStockApplicationTests.java
+++ src/test/java/com/iexapis/cloud/integration/concurrencystock/ConcurrencyStockApplicationTests.java
@@ -1,9 +1,20 @@
 package com.iexapis.cloud.integration.concurrencystock;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                MongoAutoConfiguration.class,
+                MongoDataAutoConfiguration.class
+        }
+)
 class ConcurrencyStockApplicationTests {
 
     @Test
