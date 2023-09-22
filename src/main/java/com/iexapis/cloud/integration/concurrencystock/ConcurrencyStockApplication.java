751a1bdfda0bbf61bb1e0fffbf42cfdff144ff84
Raptil <Hurricane095@gmail.com>
2023-09-22 19:55:17 +0300

    3 Issues - created rest controller for getting quotes for exactly stock

--- src/main/java/com/iexapis/cloud/integration/concurrencystock/ConcurrencyStockApplication.java
+++ src/main/java/com/iexapis/cloud/integration/concurrencystock/ConcurrencyStockApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(
         exclude = {
                 DataSourceAutoConfiguration.class,
