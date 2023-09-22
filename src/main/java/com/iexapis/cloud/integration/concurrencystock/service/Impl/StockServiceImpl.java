48e66d18ae2be8050a66542b4d783a15f025a89b
Raptil <Hurricane095@gmail.com>
2023-09-23 00:16:27 +0300

    10 Issues - add parallel stream to invoke quote api

--- src/main/java/com/iexapis/cloud/integration/concurrencystock/service/Impl/StockServiceImpl.java
+++ src/main/java/com/iexapis/cloud/integration/concurrencystock/service/Impl/StockServiceImpl.java
@@ -1,8 +1,10 @@
 package com.iexapis.cloud.integration.concurrencystock.service.Impl;
 
+import com.iexapis.cloud.integration.concurrencystock.application.port.input.QuoteClientApi;
 import com.iexapis.cloud.integration.concurrencystock.application.port.input.StockClientApi;
 import com.iexapis.cloud.integration.concurrencystock.service.StockService;
 import jakarta.annotation.PostConstruct;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,15 +19,30 @@ public class StockServiceImpl implements StockService {
 
     private final StockStorage stockStorage;
 
-    private final StockClientApi stockClientProvider;
+    private final StockClientApi stockClientApi;
 
-    public StockServiceImpl(StockStorage stockStorage, StockClientApi stockClientProvider) {
+    private final QuoteClientApi quoteClientapi;
+
+    private final QuoteStorage quoteStorage;
+
+    public StockServiceImpl(StockStorage stockStorage,
+                            QuoteStorage quoteStorage,
+                            StockClientApi stockClientProvider,
+                            QuoteClientApi quoteClientProvider) {
         this.stockStorage = stockStorage;
-        this.stockClientProvider = stockClientProvider;
+        this.quoteStorage = quoteStorage;
+        this.stockClientApi = stockClientProvider;
+        this.quoteClientapi = quoteClientProvider;
     }
 
     @PostConstruct
     public void pullStocks() {
-        stockStorage.setStocksList(stockClientProvider.getStocks());
+        stockStorage.setList(stockClientApi.getStocks());
+    }
+
+    @Scheduled(fixedRate = 10000)
+    private void invokeQuote() {
+        stockStorage.getList().parallelStream().forEach(stock ->
+                quoteStorage.addToList(quoteClientapi.getQuotesByStocks(stock.getSymbol())));
     }
 }
--- src/main/java/com/iexapis/cloud/integration/concurrencystock/service/Storage.java
+++ src/main/java/com/iexapis/cloud/integration/concurrencystock/service/Storage.java
@@ -1,6 +1,7 @@
 package com.iexapis.cloud.integration.concurrencystock.service;
 
 import java.util.Collection;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Storage
@@ -10,9 +11,11 @@ import java.util.Collection;
  * 22.09.2023
  */
 public interface Storage<T> {
-    void setStocksList(Collection<T> entityList);
+    void setList(Collection<T> entityList);
 
-    void addToStockList(Collection<T> entityList);
+    void addToList(Collection<T> entityList);
 
-    void addToStockList(T entity);
+    void addToList(T entity);
+
+    CopyOnWriteArrayList<T> getList();
 }
