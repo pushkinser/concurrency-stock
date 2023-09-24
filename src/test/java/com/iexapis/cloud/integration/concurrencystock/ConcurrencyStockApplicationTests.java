16d998e0091bdf1ae283f84d2829b9cfe38f4a96
Raptil <Hurricane095@gmail.com>
2023-09-24 19:33:03 +0300

    test mock bean mongo

--- src/test/java/com/iexapis/cloud/integration/concurrencystock/ConcurrencyStockApplicationTests.java
+++ src/test/java/com/iexapis/cloud/integration/concurrencystock/ConcurrencyStockApplicationTests.java
@@ -1,11 +1,13 @@
 package com.iexapis.cloud.integration.concurrencystock;
 
+import com.iexapis.cloud.integration.concurrencystock.application.port.persistence.QuoteNoSqlApi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 @EnableAutoConfiguration(
@@ -16,6 +18,9 @@ import org.springframework.boot.test.context.SpringBootTest;
         }
 )
 class ConcurrencyStockApplicationTests {
+
+    @MockBean
+    QuoteNoSqlApi quoteNoSqlApi;
 
     @Test
     void contextLoads() {
